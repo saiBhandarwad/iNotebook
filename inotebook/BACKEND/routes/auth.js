37a1a20c04f4f9b8e4e9190996f1f08547f1f7ca
saiBhandarwad <mrsaibhandarwad@gmail.com>
2023-08-09 10:10:42 +0530

    jwt, hash, validation added

--- inotebook/BACKEND/routes/auth.js
+++ inotebook/BACKEND/routes/auth.js
@@ -1,20 +1,60 @@
 const express = require('express')
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
+const JWT_SIGNATURE = 'saiisagoodboy'
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send('hi this is auth slash endpoint')
 })
 
 //creating a new user (doesn't require authentication)
-router.post('/',(req,res)=>{
+
+router.post('/signUp', [
+    body('name', 'Enter a valid name and min length should be 3').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'password length must be 6 character').isLength({ min: 6 }),
+], async (req, res) => {
     try {
-        const user = User(req.body)
-        user.save()
-        res.send(req.body)
+        const result = validationResult(req)
+        //if any errors are occured
+        if (!result.isEmpty()) {
+            return res.status(401).json({ error: result.array() })
+        }
+        //check whether a user already exist or not
+        const user = await User.findOne({ email: req.body.email })
+        if (user) {
+            res.status(500).send({ msg: "email already exist" })
+        } else {
+            const salt = await bcrypt.genSalt(10)
+            const secPass = await bcrypt.hash(req.body.password, salt)
+            const response = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass,
+            })
+            const data = {
+                user: { id: response.id }
+            }
+            const authToken = jwt.sign(data, JWT_SIGNATURE)
+            res.json({authToken})
+        }
     } catch (error) {
-        console.log('error occured in post at auth.js while creating user');
+        res.status(500).send('internal error occured')
     }
+})
+
+//login endpoint
+
+router.post('/login', [
+    body('email', 'please provide valid email or email is not registered yet').isEmail(),
+    body('password', 'password can not be blank').exists()
+], async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+
+    res.send(user)
 })
 
 module.exports = router
